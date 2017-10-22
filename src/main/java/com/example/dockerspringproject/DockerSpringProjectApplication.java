aeca34db03631ef5ef3b8e421da7bb3fa8fc5b5c
unknown <egiants.git@gmail.com>
2017-10-22 12:55:12 -0500

    sai

---
+++ src/main/java/com/example/dockerspringproject/DockerSpringProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.dockerspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringProjectApplication.class, args);
+	}
+}
